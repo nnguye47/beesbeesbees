45150b83702355cb331bc92a4956c6047f21edfa
Nam Nguyen <nam.nguyen201@gmail.com>
2023-02-13 11:54:06 -0800

    Pass all tests up to ForagerBee.js

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,18 @@
 var Bee = function() {
+  //create the bee class
+
+  //call the superclass Grub
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
+
+//create a bee prototype
+//point it to the grub prototype
+Bee.prototype = Object.create(Grub.prototype);
+
+//create a constructor and set it to the grub constructor
+Bee.prototype.constructor = Bee;
+
 
